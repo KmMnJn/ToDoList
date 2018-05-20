610652e5e6a0699b0842e266ee0f51f947b993a6
Minjun Kim <31723018+KmMnJn@users.noreply.github.com>
2018-05-21 01:07:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def create_db():
 
 	table_create_sql = """create table if not exists todo (
 				id integer primary key autoincrement,
-				what text not null,
+				title text not null,
 				due text not null,
 				important integer,
 				finish integer,
@@ -25,13 +25,52 @@ def create_db():
 	cur.execute(table_create_sql)# Create table.
 	conn.close()# Close Connection.
 
-tree = []
-a = Node(0)
-a.addChildren(5)
-a.addChildren(9)
-a.addChildren(11)
-a.addChildren(14)
+def create_tree():
+	import sqlite3
+	conn = sqlite3.connect("todolist.db")# Create DB file.
+	cur = conn.cursor()# Create cursor on Connection.
 
-a.deleteChild(11)
 
-print(a.children)
+	cur.execute(table_create_sql)# Create table.
+	conn.close()# Close Connection.
+
+def add_item(_parent):
+	import sqlite3
+	conn = sqlite3.connect("todolist.db")# Create DB file.
+	cur = conn.cursor()# Create cursor on Connection.
+	(_title, _due, _important) = 
+	cur.execute('''INSERT INTO todo (title, due, important, finish, parent) values (?, ?, ?, ?, ?)''', (_title, _due, int(_important), 0, _parent))
+	conn.close()# Close Connection.
+
+	tree[_parent].addChildren(len(tree))
+	tree.append(Node(_parent))
+
+def del_item(_parent):
+	import sqlite3
+	conn = sqlite3.connect("todolist.db")# Create DB file.
+	cur = conn.cursor()# Create cursor on Connection.
+	(_title, _due, _important) = 
+	conn.close()# Close Connection.
+
+	calculate_list = []
+	tree[child] = 0
+	tree[_parent].deleteChild(child)
+
+def modify_item(_childId):
+	import sqlite3
+	conn = sqlite3.connect("todolist.db")# Create DB file.
+	cur = conn.cursor()# Create cursor on Connection.
+	cur.execute('''UPDATE todo SET what = ?, due = ?, finished = ? WHERE ID = ?''', (wh, du, Fi, Rid))
+	conn.close()# Close Connection.
+
+def move_item(_childId):
+	
+
+def view_item(_parent):
+	import sqlite3
+	conn = sqlite3.connect("todolist.db")# Create DB file.
+	cur = conn.cursor()# Create cursor on Connection.
+
+	conn.close()# Close Connection.
+
+tree = [Node(-1)]
